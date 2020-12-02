02b249797466c94815642c04d195bf684b8714b9
abdeldev007 <52884782+abdeldev007@users.noreply.github.com>
2020-12-02 21:45:21 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -301,14 +301,13 @@ $(document).ready(function () {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
 /*
 	Solid State by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-function($) {
+(function($) {
 
 	var	$window = $(window),
 		$body = $('body'),
